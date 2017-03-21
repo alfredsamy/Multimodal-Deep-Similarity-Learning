bdffcf5dd605be1b5a9de94bdf2d0e0d7a1201a2
robert <robert1826@gmail.com>
2017-03-21 10:04:22 +0200

    added core functionality (not running yet)

--- cbir.py
+++ cbir.py
@@ -59,29 +59,172 @@ def load_data():
 
 gist_desc, surf_desc, sift_desc, gabor_desc , lbp_desc, labels = load_data()
 
-def generate_batch(query_img_features):
+labels_index = {}
+labels_sum = {}
+
+sum = 0
+labl = labels[0]
+labels_index[labl] = 0
+for i in range(len(labels)):
+	if(labl != labels[i]):
+		print(labl, sum)
+		labels_sum[labl] = sum
+		labl = labels[i]
+		sum = 0
+		labels_index[labl] = i
+	else:
+		sum += 1
+labels_sum[labl] = sum
+
+print(labels_sum)
+print(labels_index)
+print()
+
+
+test_gist_desc = []
+test_surf_desc = []
+test_sift_desc = []
+test_gabor_desc = []
+test_lbp_desc = []
+test_labels = []
+for k,v in labels_index.items():
+	print(k,v)
+	start = v
+	end = v + max(1,labels_sum[k]//10)
+	for i in gist_desc[start:end]:
+		test_gist_desc.append(i)
+	for i in surf_desc[start:end]:
+		test_surf_desc.append(i)
+	for i in sift_desc[start:end]:
+		test_sift_desc.append(i)
+	for i in gabor_desc[start:end]:
+		test_gabor_desc.append(i)
+	for i in lbp_desc[start:end]:
+		test_lbp_desc.append(i)
+	for i in labels[start:end]:
+		test_labels.append(i)
+	del gist_desc[start:end]
+	del surf_desc[start:end]
+	del sift_desc[start:end]
+	del gabor_desc[start:end]
+	del lbp_desc[start:end]
+	del labels[start:end]
+	
+	sum = 0
+	labels_index = {}
+	labels_sum = {}
+	labl = labels[0]
+	labels_index[labl] = 0
+	for i in range(len(labels)):
+		if(labl != labels[i]):
+			labels_sum[labl] = sum
+			labl = labels[i]
+			sum = 0
+			labels_index[labl] = i
+		else:
+			sum += 1
+	labels_sum[labl] = sum
+
+print(labels_sum)
+print(labels_index)
+print("*************************Data Loaded**********************************")
+
+###################################################################################################
+#Graph:
+num_labels = 10
+batch_size = 18
+num_hidden_nodes = 100
+num_hidden_nodes2 = 50
+
+#init weight for different network
+def init_weight(input_size):
+	w1 = tf.Variable(tf.truncated_normal([input_size, num_hidden_nodes],stddev=0.1))
+	b1 = tf.Variable(tf.zeros([num_hidden_nodes]))
+	
+	w2 = tf.Variable(tf.truncated_normal([num_hidden_nodes, num_hidden_nodes2],stddev=0.1))
+	b2 = tf.Variable(tf.zeros([num_hidden_nodes2]))
+		
+	w3 = tf.Variable(tf.truncated_normal([num_hidden_nodes2, num_hidden_nodes2],stddev=0.1))
+	b3 = tf.Variable(tf.zeros([num_hidden_nodes2]))
+
+	w4 = tf.Variable(tf.truncated_normal([num_hidden_nodes2, num_hidden_nodes2],stddev=0.1))
+	b4 = tf.Variable(tf.zeros([num_hidden_nodes2]))
+
+	w5 = tf.Variable(tf.truncated_normal([num_hidden_nodes2, num_hidden_nodes2],stddev=0.1))
+	b5 = tf.Variable(tf.zeros([num_hidden_nodes2]))
+	#return w1,b1,w2,b2,w3,b3,w4,b4,w5,b5
+	return (w1,w2,w3,w4,w5),(b1,b2,b3,b4,b5)
+
+
+
+graph = tf.Graph()
+with graph.as_default():
+	tf_train_gist = tf.placeholder(tf.float32,shape=(batch_size, 2*960))
+	tf_train_sift = tf.placeholder(tf.float32,shape=(batch_size, 2*200))
+	tf_train_surf = tf.placeholder(tf.float32,shape=(batch_size, 2*200))
+	#tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size))
+	
+	tf_test_gist = tf.constant(np.array(test_gist_desc))
+	tf_test_surf = tf.constant(np.array(test_surf_desc))
+	tf_test_sift = tf.constant(np.array(test_sift_desc))
+	global_step = tf.Variable(0)
+
+	# Init Variables.
+	gist_w, gist_b = init_weight(2*960)
+	surf_w, surf_b = init_weight(2*200)
+	sift_w, sift_b = init_weight(2*200)
+	sum_w = tf.Variable(tf.truncated_normal([3*num_hidden_nodes2, 1],stddev=0.1))
+	sum_b = tf.Variable(tf.zeros([1]))
+	# Training computation.
+	def model(data,W,B):
+		lay1 = tf.nn.relu(tf.matmul(data, W[0]) + B[0])
+		lay2 = tf.nn.relu(tf.matmul(lay1, W[1]) + B[1])
+		lay3 = tf.nn.relu(tf.matmul(lay2, W[2]) + B[2])
+		lay4 = tf.nn.relu(tf.matmul(lay3, W[3]) + B[3])
+		return tf.nn.relu(tf.matmul(lay4, W[4]) + B[4])
+	
+	gist_logits = model(tf_train_gist, gist_w, gist_b)
+	surf_logits = model(tf_train_sift, surf_w, surf_b)
+	sift_logits = model(tf_train_sift, sift_w, sift_b)
+	
+	concat = tf.stack([gist_logits, surf_logits, sift_logits], axis=1)
+	concat = tf.reshape(concat, [batch_size,150]) 
+	similarity = tf.matmul(concat, sum_w) + sum_b
+	similarity = tf.reshape(similarity,(1,-1))[0]
+	
+	#Calculate the loss
+	L = []
+	for i in range(batch_size):#similarity[i+1] ==> x,x-
+		a = similarity[i]
+		i += 1
+		if(i < batch_size):
+			b = similarity[i]
+			s = 1 + b - a
+			r = 0
+			r = tf.cond(s>0,lambda: s,lambda: tf.add(0.,0.))
+			L.append(r)
+			#L += max(0, s)
+
+	loss = tf.reduce_mean(L)
+	#loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+	
+	# Optimizer.
+	optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+	# learning_rate = tf.train.exponential_decay(0.5, global_step, 1000, 0.65, staircase=True)
+	# optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+
+########################################################################################################
+
+
+def generate_batch(query_img_features, cur_img_ind):
 	train_gist = []
 	train_sift = []
 	train_surf = []
-	for k,v in labels_index.items():
-		start = v
-		end = v + labels_sum[k]
-		offset1 = random.randint(start,end-1)
-		offset2 = offset1
-		while(offset2 == offset1):
-			offset2 = random.randint(start,end-1)
-		offset3 = start
-		while(offset3>=start and offset3<end):
-			offset3 = random.randint(0,len(labels)-1)
-		
-		train_gist.append(np.concatenate((gist_desc[offset1] , gist_desc[offset2]),axis=0))
-		train_sift.append(np.concatenate((sift_desc[offset1][0], sift_desc[offset2][0]),axis=0))
-		train_surf.append(np.concatenate((surf_desc[offset1][0], surf_desc[offset2][0]),axis=0))
 
-		train_gist.append(np.concatenate((gist_desc[offset1], gist_desc[offset3]),axis=0))
-		train_sift.append(np.concatenate((sift_desc[offset1][0], sift_desc[offset3][0]),axis=0))
-		train_surf.append(np.concatenate((surf_desc[offset1][0], surf_desc[offset3][0]),axis=0))
-		
+	train_gist.append(np.concatenate((gist_desc[cur_img_ind] , query_img_features['gist']),axis=0))
+	train_sift.append(np.concatenate((sift_desc[cur_img_ind][0], query_img_features['sift']),axis=0))
+	train_surf.append(np.concatenate((surf_desc[cur_img_ind][0], query_img_features['surf']),axis=0))
+
 	return np.array(train_gist), np.array(train_sift), np.array(train_surf)
 
 
@@ -101,22 +244,21 @@ bowDiction_surf = load_surf_bow_diction('Surf_Voc')
 print('Done Surf dict')
 
 query_img_features = gen_query_features(misc.imread(query_img_file), bowDiction_sift, bowDiction_surf)
-print(query_img_features)
+print('Generated Query Image Features')
 
 
-# saver = tf.train.Saver()
-# with tf.Session(graph=graph) as session:
-# 	saver.restore(sess, "model.ckpt")
-# 	print("Loaded Model")
-	
+with tf.Session(graph=graph) as sess:
+	saver = tf.train.Saver()
+	saver.restore(sess, "model.ckpt")
+	print("Model Loaded")
+
+	for i in len(gist_desc):
+		a,b,c = generate_batch(query_img_features, i)
+		# print("***************************",a.shape,b.shape,c.shape)
+		# Prepare a dictionary telling the session where to feed the minibatch.
+		feed_dict = {tf_train_gist: a, tf_train_sift: b,tf_train_surf: c}
+		sim, l, _ = session.run([similarity], feed_dict=feed_dict)
+		print(i, 'Sim =', sim, 'l =', l)
 
 
-# 	for i in len(gist_desc):
-
-# 		a,b,c = generate_batch()
-# 		# print("***************************",a.shape,b.shape,c.shape)
-# 		# Prepare a dictionary telling the session where to feed the minibatch.
-# 		feed_dict = {tf_train_gist: a, tf_train_sift: b,tf_train_surf: c}
-# 		sim, l, _ = session.run([similarity], feed_dict=feed_dict)
-	
-# print("DONE")
+print("DONE")
